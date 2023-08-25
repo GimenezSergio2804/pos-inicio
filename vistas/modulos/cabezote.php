6ca42838b94ed2fdb2c2989358afa6a253409185
Sergio Alejandro Gimenez <sergiogimenez2804@gmail.com>
2023-08-25 10:20:46 -0300

    Primer etapa, aun sin concluir

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,71 @@
+<header class="main-header">
+
+    <!-- ===========================================
+                        HEADER - inicio
+ ===========================================-->
+
+    <!-- Logo -->
+    <a href="inicio" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>M</b>CS</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>Markito</b>Custom Shop</span>
+        <!-- <img src="" alt="" class="img-responsive" style="padding:10px"> -->
+    </a>
+
+    <!-- ===========================================
+                        HEADER - fin
+    ===========================================-->
+
+
+    <!-- ===========================================
+                BARRA de NAVEGACION - inicio
+    =============================================-->
+
+    <nav class="navbar navbar-static-top">
+
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+            <!-- <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span> -->
+        </a>
+
+        <!-- Perfil de usuario -->
+        <div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+
+                <li class="dropdown user user-menu">
+
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+                        <img src="vistas/img/default/anonymous.png" alt="foto de usuario" class="user-image">
+
+                        <span class="hidden-xs">Usuario Administrador</span>
+
+                    </a>
+
+                    <!-- Dropdown togle -->
+
+                    <ul class="dropdown-menu">
+
+                        <li class="user-body">
+
+                            <div class="pull-right">
+                                <a href="" class="btn btn-default btn-flat">Salir</a>
+                            </div>
+
+                        </li>
+
+                    </ul>
+
+                </li>
+
+            </ul>
+
+        </div>
+
+    </nav>
+</header>
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+
+    <strong> Copyright &copy; <?php date("y") ?> <a href="www.catmoon.com" target="_blank">Catmoon</a> </strong>
+    Todos los derechos Reservados
+
+</footer>
